8172f8891d69309eb63b492628d780f0c5999bef
Anith Prakash <64299704+AnithPrakash@users.noreply.github.com>
2024-01-17 02:57:06 +0530

    Update model.py

--- model.py
+++ model.py
@@ -14,31 +14,31 @@ with col2:
     sex=st.text_input("Enter your Sex (1 or 0)")
 
 with col3:
-    cp=st.text_input("Enter your Chest Pain types ( level=1/2/3/0 ) ")
+    cp=st.text_input("Enter your Chest Pain types")
 
 with col1:
     tresrbps=st.text_input("Enter your Resting Blood Pressure")
 
 with col2:
-    chol=st.text_input("Enter your Serum Cholestoral in mg/dl")
+    chol=st.text_input("Enter your Serum Cholestoral ")
 
 with col3:
-    fbs=st.text_input("enter your Fasting Blood Sugar > 120 mg/dl")
+    fbs=st.text_input("Fasting Blood Sugar > 120 mg/dl")
 
 with col1:
-    restecg=st.text_input("enter your Resting Electrocardiographic results")
+    restecg=st.text_input("enter your ECG")
 
 with col2:
-    thalach=st.text_input("Enter your Maximum Heart Rate achieved")
+    thalach=st.text_input("Enter your Maximum Heart Rate ")
 
 with col3:
     exang=st.text_input("Enter your Exercise Induced Angina")
 
 with col1:
-    oldpeak=st.text_input("ST depression induced by exercise)")
+    oldpeak=st.text_input("ST depression induced by exercise /n ")
 
 with col2:
-    slope=st.text_input("Enter your Slope of the peak exercise ST segment ( 0 / 1 / 2)")
+    slope=st.text_input("Enter your Slope of the peak exercise /n ( 0 / 1 / 2)")
 
 with col3:
     cs=st.text_input("Enter your Major vessels colored by flourosopy (0 / 1 / 2)")
